e2b35f3290d56ddca59f94d8f303e0de084b5d65
hinnefe2 <hhinnefeld@civisanalytics.com>
2017-11-11 14:00:29 -0600

    ENH Add if/else to check if envvar defined

--- brewlog/db_io.py
+++ brewlog/db_io.py
@@ -78,7 +78,11 @@ def read_last_brew():
     params = {}
 
     parse.uses_netloc.append("postgres")
-    url = parse.urlparse(os.environ["DATABASE_URL"])
+
+    if os.getenv("DATABASE_URL") is None:
+        return None
+    else:
+        url = parse.urlparse(os.getenv("DATABASE_URL"))
 
     with pg2.connect(database=url.path[1:], user=url.username,
                      password=url.password, host=url.hostname, port=url.port) as conn:
