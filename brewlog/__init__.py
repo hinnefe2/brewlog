fe3e2e095effcbf85af555858f56789e16f6e258
Henry Hinnefeld <henry.hinnefeld@gmail.com>
2018-04-15 14:40:47 -0500

    ENH Add LoginManager to __init__.py

--- brewlog/__init__.py
+++ brewlog/__init__.py
@@ -1,4 +1,23 @@
+import os
+
 from flask import Flask
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+# allow non-https oauth callback
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+app.config["SECRET_KEY"] = os.getenv("BREWLOG_SECRET_KEY")
+
 from brewlog import views
+from brewlog.login import User
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.session_protection = None
+
+
+@login_manager.user_loader
+def load_user(user_id):
+        return User(user_id)
