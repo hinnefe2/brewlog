86361cb28f92af4bb2ec8a56f8e6cbe00a1337cb
hinnefe2 <hinnefe2@illinois.edu>
2018-07-22 10:47:22 -0500

    Add config.py so we can import it (d'oh)

---
+++ brewlog/config.py
@@ -0,0 +1,26 @@
+APP_CONFIG = {
+    'recipe': [
+        {'name': 'roaster', 'descr': 'Roaster', 'type': 'text'},
+        {'name': 'beans', 'descr': 'Beans', 'type': 'text'},
+        {'name': 'roast_date', 'descr': 'Roast date (MM/DD/YY)', 'type': 'text'},
+        {'name': 'coffee', 'descr': 'Coffee (g)', 'type': 'number'},
+        {'name': 'grind', 'descr': 'Grind', 'type': 'number'},
+        {'name': 'temp', 'descr': 'Temperature (F)', 'type': 'number'},
+        {'name': 'water', 'descr': 'Water (g)', 'type': 'number'}
+        ],
+    'steps': [
+        {'name': 'wait_cool', 'descr': 'water cool'},
+        {'name': 'pour', 'descr': 'pour'},
+        {'name': 'wait_bloom', 'descr': 'wait'},
+        {'name': 'stir', 'descr': 'stir'},
+        {'name': 'wait_stir', 'descr': 'wait'},
+        {'name': 'press', 'descr': 'press'}
+        ],
+    'scores': [
+        {'name': 'sour', 'descr': 'Sourness'},
+        {'name': 'bitter', 'descr': 'Bitterness'},
+        {'name': 'strength', 'descr': 'Strength'},
+        {'name': 'smell', 'descr': 'Smell'},
+        {'name': 'finish', 'descr': 'Finish'},
+        {'name': 'overall', 'descr': 'Overall'}
+        ]}
