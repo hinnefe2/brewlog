2455128a10ae0ca4e2f6436eae75ed501ad93b9b
hinnefe2 <hinnefe2@illinois.edu>
2018-07-21 20:01:31 -0500

    Add sqlalchemy imports and config to __init__.py

--- brewlog/__init__.py
+++ brewlog/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -9,9 +10,13 @@ app = Flask(__name__)
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app.config["SECRET_KEY"] = os.getenv("BREWLOG_SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+# create the sqlalchemy database object
+db = SQLAlchemy(app)
 
 from brewlog import views
-from brewlog.login import User
+from brewlog.models import User
 
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
@@ -20,4 +25,4 @@ login_manager.session_protection = None
 
 @login_manager.user_loader
 def load_user(user_id):
-        return User(user_id)
+    return User.query.filter_by(user_id = int(user_id)).first()
