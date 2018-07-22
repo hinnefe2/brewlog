31e4325acab3679663a186bc0a4b32794c99df6b
hinnefe2 <hinnefe2@illinois.edu>
2018-07-21 20:00:48 -0500

    Remove placeholder User class in login.py

--- brewlog/login.py
+++ brewlog/login.py
@@ -6,18 +6,12 @@ from requests_oauthlib import OAuth2Session
 
 CLIENT_ID = "708447731611-rtlh8cgdutkt1mnjp5psug6uiu8npfg6.apps.googleusercontent.com"  # noqa
 REDIRECT_URI = "https://aerobrewlog.herokuapp.com/gCallback"
+REDIRECT_URI_DEV = "http://localhost:5000/gCallback"
 AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
 TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
 SCOPE = ["https://www.googleapis.com/auth/userinfo.email",
          "https://www.googleapis.com/auth/plus.me"]
 CLIENT_SECRET = os.getenv("BREWLOG_SECRET_KEY")
-
-
-class User(UserMixin):
-    """Class to hold user information for flask-login"""
-
-    def __init__(self, id_):
-        self.id = id_
 
 
 def get_google_auth():
