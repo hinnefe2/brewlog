d892b29921c5a7a455edf0599501e408bc74d884
hinnefe2 <hinnefe2@illinois.edu>
2020-07-18 21:05:43 -0500

    Remove Dash related code

--- brewlog/__init__.py
+++ brewlog/__init__.py
@@ -1,38 +1,11 @@
 import os
 
-import matplotlib
-
-from flask import Flask, render_template
-from flask_login import LoginManager, login_required
+from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-
-from dash import Dash
 
 
 app = Flask(__name__)
-
-# need to set the mpl backend before we use pyplot anywhere
-# see https://stackoverflow.com/questions/2801882/generating-a-png-with-matplotlib-when-display-is-undefined  # noqa
-matplotlib.use('Agg')
-
-
-class MyDash(Dash):
-
-    def interpolate_index(self, metas='', title='', css='', config='',
-                          scripts='', app_entry='', favicon=''):
-        return render_template('flask_app.html', scripts=scripts,
-                               config=config, app_entry=app_entry)
-
-    def add_url(self, name, view_func, methods=('GET',)):
-        self.server.add_url_rule(
-            name,
-            view_func=login_required(view_func),
-            endpoint=name,
-            methods=list(methods)
-        )
-
-
-dash_app = MyDash(server=app, url_base_pathname='/analyze/')
 
 # allow non-https oauth callback
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
@@ -43,9 +16,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # create the sqlalchemy database object
 db = SQLAlchemy(app)
-
-# this module sets up the dash layout and callbacks
-from brewlog import analyze  # noqa
 
 from brewlog import views  # noqa
 from brewlog.models import User  # noqa
